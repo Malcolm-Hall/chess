3c509dbe10315ad48bbc9a619cfebeddfe9db8e3
Malcolm-Hall <malcolm.r.hall@outlook.com>
2021-09-24 14:28:31 +0100

    Prompt users to load a postion from FEN

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import pyglet
 import gui.game
 
 if __name__ == '__main__':
-    main = gui.game.Main("rnbqkbnr/pppppppp/PP6/PP6/8/PPPP4/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    # def update(dt):
-    #     main.on_draw()
-    #
-    # pyglet.clock.schedule_interval(update, 1 / 1.0)
+    #main = gui.game.Main("rnbqkbnr/pppppppp/PP6/PP6/8/PPPP4/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    fen = input("Enter FEN to start from a position or leave empty for a new game: ")
+    if fen:
+    	main = gui.game.Main(fen)
+    else:
+    	main = gui.game.Main()
     pyglet.app.run()
